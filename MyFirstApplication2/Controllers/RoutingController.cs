b8dd11f25414d3295df47442fa3c33eb3ef9ced6
hira.majeed <hira.majeed@softnology.com>
2023-09-19 10:49:45 +0500

    changes

---
+++ MyFirstApplication2/Controllers/RoutingController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace MyFirstApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoutingController : ControllerBase
+    {
+      
+    }
+}
